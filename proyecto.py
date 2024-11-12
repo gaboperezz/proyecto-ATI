827cc86ccb5c0cc659b5bb3c2351ee8a3a4156e2
Gonzalo Ulivi <gonzaulivi@gmail.com>
2024-11-12 19:48:06 -0300

    MEJORANDO PA

--- proyecto.py
+++ proyecto.py
@@ -8,8 +8,24 @@ customtkinter.set_default_color_theme("dark-blue")
 root = customtkinter.CTk()
 root.geometry("500x350")
 
+#Datos para simular la base de datos#
+class Usuario:
+    def __init__(self, nombre, contrasena):
+        self.nombre = nombre
+        self.contrasena = contrasena
+    
+usuario1 = Usuario("chinac", "elchelo")
+usuario2 = Usuario("zgabo4", "lostussienvelez")
+
+usuarios = {usuario1, usuario2}
+
+
 def login():
-    print("HOLA")
+    for u in usuarios:
+        if(entry1.get() == u.nombre and entry2.get() == u.contrasena): 
+            print("SI")
+            break
+    else: print("NO")
 
 def openFile():
     filepath = filedialog.askopenfilename()
@@ -19,6 +35,16 @@ def openFile():
     print(text)
 
 
+def registrarse():
+    frame.pack_forget()
+    frameRegistro.pack(pady=20, padx=60, fill="both", expand=True)
+
+def confirmarRegistro():
+    print("HOLA")
+
+
+    
+# FRAME LOGIN #
 frame = customtkinter.CTkFrame(master=root)
 frame.pack(pady=20, padx=60, fill="both", expand=True)
 
@@ -34,10 +60,29 @@ entry2.pack(pady=12, padx=10)
 button = customtkinter.CTkButton(master=frame, text="Login", command=login)
 button.pack(pady=12, padx=10)
 
+button = customtkinter.CTkButton(master=frame, text="No tengo cuenta", command=registrarse)
+button.pack(pady=12, padx=135)
+
 checkBox = customtkinter.CTkCheckBox(master=frame, text="Remember Me")
 checkBox.pack(pady=12, padx=10)
 
 button2 = customtkinter.CTkButton(master=frame, text="Adjuntar archivo",command=openFile)
 button2.pack(pady=15, padx=12)
+
+# FRAME REGISTRO #
+frameRegistro = customtkinter.CTkFrame(master=root)
+
+labelRegistro = customtkinter.CTkLabel(master=frameRegistro, text="Registrarse")
+labelRegistro.pack(pady=12, padx=10)
+
+entry1Registro = customtkinter.CTkEntry(master=frameRegistro, placeholder_text="Username")
+entry1Registro.pack(pady=12, padx=10)
+
+entry2Registro = customtkinter.CTkEntry(master=frameRegistro, placeholder_text="Password", show="*")
+entry2Registro.pack(pady=12, padx=10)
+
+buttonRegistro = customtkinter.CTkButton(master=frameRegistro, text="Confirmar", command=confirmarRegistro)
+buttonRegistro.pack(pady=12, padx=10)
+
 
 root.mainloop()
